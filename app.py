82094e183ab7d41a2678a9846a86d50576c3689f
Gauravdarkslayer <www.gaurav10bhojwani@gmail.com>
2020-02-22 13:35:09 +0530

    database connectivity added

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask,render_template,flash, redirect,url_for,session,logging,
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Wolverine/Desktop/addiction project/flask-login-register-form-master/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Wolverine/Desktop/addiction_project/database.db'
 db = SQLAlchemy(app)
 
 
 class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
+    # username = db.Column(db.String(80))
     email = db.Column(db.String(120))
     password = db.Column(db.String(80))
 
@@ -21,13 +21,13 @@ def index():
 @app.route("/login",methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        uname = request.form["uname"]
+        mail = request.form["mail"]
         passw = request.form["passw"]
         
-        login = user.query.filter_by(username=uname, password=passw).first()
+        login = user.query.filter_by(email=mail, password=passw).first()
         if login is not None:
-            return redirect(url_for("index"))
-    return render_template("login.html")
+            return "Success in login"
+    return render_template("login.html",["login failed"])
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
